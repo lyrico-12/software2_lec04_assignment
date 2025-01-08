62c081556bca887c2942f3598eef93a1dcddc92a
lyrico-12 <lyrico1202@gmail.com>
2025-01-08 22:59:03 +0900

    README.mdを編集

--- adv/src/adv_regression.c
+++ adv/src/adv_regression.c
@@ -40,20 +40,6 @@ int main() {
     standerdization(train_len, traindata, &train_y_mean, &train_y_std);
     standerdization(test_len, testdata, &test_y_mean, &test_y_std);
 
-    // for (int i = 0; i < train_len; i++) {
-    //     printf("traindata[%d]: ", i);
-    //     for (int j = 0; j < 5; j++) {
-    //         printf("%7.4f, ",traindata[i][j]);
-    //     }
-    //     printf("\n");
-    // }
-    // for (int i = 0; i < test_len; i++) {
-    //     printf("testdata[%d]: ", i);
-    //     for (int j = 0; j < 5; j++) {
-    //         printf("%7.4f, ",testdata[i][j]);
-    //     }
-    //     printf("\n");
-    // }
 
     const double alpha = 0.01;
 
@@ -66,10 +52,10 @@ int main() {
     const int iter1 = optimize(a1, alpha, train_len, traindata, gradient, sse_value);
     double rss1 = calc_rss(a1, train_len, traindata);
     double tss1 = calc_tss(train_len, traindata);
-    double r2_1 = calc_cod(rss1, tss1);
+    double r2_1 = calc_cod(rss1, tss1);// train datasetの決定係数
 
     // testdataの回帰分析
-    printf("\n-----linear regression of testdata-----\n");
+    printf("-----linear regression of testdata-----\n");
     double a2[4];
     for (int i = 0; i < 4; i++) {
         a2[i] = 1.0;
@@ -77,14 +63,15 @@ int main() {
     const int iter2 = optimize(a2, alpha, test_len, testdata, gradient, sse_value);
     double rss2 = calc_rss(a2, test_len, testdata);
     double tss2 = calc_tss(test_len, testdata);
-    double r2_2 = calc_cod(rss2, tss2);
+    double r2_2 = calc_cod(rss2, tss2);// test datasetの決定係数
 
-
+    // 分析の結果表示。目的変数の予測値と実測値、またその差の絶対値を表示
     printf("\n-------result of train data-------\n");
     print_result(a1, train_len, traindata, train_y_mean, train_y_std, train_y);
     printf("\n-------result of test data-------\n");
     print_result(a2, test_len, testdata, test_y_mean, test_y_std, test_y);
 
+    // 重回帰分析モデルの評価。最適化時のイテレーション、決定係数を表示。
     printf("\n--------regression model validation-------\n");
     printf("train data: number of iteration = %d, R^2(決定係数) = %7.4f\n", iter1, r2_1);
     printf("test data: number of iteration = %d, R^2(決定係数) = %7.4f\n", iter2, r2_2);
